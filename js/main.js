2aa3f35b867d9a6ac1254809f062a737a6854d93
Caleb Payne <43116911+caleb-payne@users.noreply.github.com>
2018-11-28 18:12:30 -0500

    favorites now working

--- js/main.js
+++ js/main.js
@@ -11,14 +11,22 @@ $(function(){
     var vimeoFrame = $('#vimeo-frame');
     var clearLockButton = $('#clear-lock-button');
     var backButton = $('#back-button');
+    var addButton = $('#add-button');
+    var deleteButton = $('.delete-button');
+    var bookmarkList = $('#bookmark-list');
+    
+    var storedBookmarks = localStorage.getItem('faves');
     
     var currentVideoNumber = ''; //string
-    var previousVideoNumber = ''; //string
+    var pareviousVideoNumber = ''; //string
+    var faveCount = 0;
     
     
     
     //***************************************
     //*************  PAGE SETUP
+    
+    console.log('string is ' + storedBookmarks);
     
     //create array of digit objects
     var digitArray = new Array(); 
@@ -28,6 +36,18 @@ $(function(){
         digitArray[i] = new Digit('-', $('.digit').eq(i), false);
     }
     
+    if (storedBookmarks != null) {
+        
+        var storedArray = storedBookmarks.split('%');
+        
+        for (var w = 0; w < storedArray.length; w++) {
+            
+            if (storedArray[w] != null) {
+                
+                addNewBookmark(storedArray[w]);
+            } 
+        }
+    }
     
     
     //***************************************
@@ -40,6 +60,12 @@ $(function(){
         return hex;
     };
     
+    
+    //random integer function
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+     
         
     function linear(incoming,bottom,top,min,max,cap) {
  
@@ -71,19 +97,6 @@ $(function(){
         this.locked = locked;
     }
     
-    function Fave(faveData) {
-        
-        this.faveURL = faveData[0];
-        this.faveSlot = $(faveData[1]);
-        this.faveColor = faveData[2];
-        this.lleno = faveData[3]=='0' ? false : true;
-    }
-    
-    //random integer function
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
-    
     
     function shuffleNumbers() {
         
@@ -110,7 +123,7 @@ $(function(){
                 //else, same number or do nothing
             }  
             
-            console.log('digit value ' + j + ' equals ' + digitObj.digitValue);
+            //console.log('digit value ' + j + ' equals ' + digitObj.digitValue);
             
             //add digit value to the new url
             newUrl = newUrl + digitObj.digitValue;
@@ -153,7 +166,12 @@ $(function(){
     
     function getFaveColor(urlNum) {
         
-        var ch = Math.floor(urlNum.length()/3);
+        //skews blue, needs some work
+        
+        urlNum = urlNum.replace(/-/g, '');
+        console.log(urlNum);
+        
+        var ch = Math.floor(urlNum.length/3);
         
         var red = parseInt(urlNum.substr(0,ch));
         var green = parseInt(urlNum.substr(ch,ch));
@@ -161,14 +179,130 @@ $(function(){
         
         var range = Math.pow(10,ch);
             
-        red = linear(red,0,range,0,255,true);
-        green = linear(green,0,range,0,255,true);
-        blue = linear(blue,0,range,0,255,true);
+        red = Math.floor(linear(red,0,range,0,255,true));
+        green = Math.floor(linear(green,0,range,0,255,true));
+        blue = Math.floor(linear(blue,0,range,0,255,true));
         
-        return '#' + rgbToHex(red) + rgbToHex(green) + rgbToHex(blue);
+        console.log(red + ' ' + green + ' ' + blue);
+        
+        var newFaveColor = '#' + rgbToHex(red) + rgbToHex(green) + rgbToHex(blue);
+        
+        return newFaveColor;
     }
      
     
+    function addNewBookmark(bookmarkNum) {
+        
+        if (faveCount < 3) {
+            
+            if (bookmarkNum == '') {
+            
+                alert('no video number yet!');
+                return;
+            }
+        
+            //add new DOM elements
+            var newPlunger = "<a class=\"fave-plunger\" href=\"" + bookmarkNum + "\">LOAD</a>"
+            var newLabel = $("<p class=\"url-label\"></p>").text(bookmarkNum);
+            var newDelete = "<div class=\"delete-button\" data-fave-url=\"" + bookmarkNum + "\"><i class=\"fas fa-trash\"></i></div>"
+
+            var newListItem = $("<li class=\"bookmark-item\" data-fave-url=\"" + bookmarkNum + "\"></li>").prependTo(bookmarkList).append(newPlunger, newLabel, newDelete);
+
+            newListItem.css('backgroundColor', getFaveColor(bookmarkNum));
+
+            faveCount ++;
+            console.log('faveCount is now ' + faveCount);
+            
+            if (faveCount >= 3) {
+            
+                addButton.addClass('add-button-hidden');
+                console.log('supposed to hide now');
+            }
+            
+        }    
+    }
+    
+    
+    function removeBookmark(trash) {
+        
+        trash.parent().remove();
+        faveCount --;
+        console.log('faveCount is now ' + faveCount);
+        
+        addButton.removeClass('add-button-hidden');
+    }
+    
+    
+    function addToLocalStorage(urlToAdd) {
+        
+        var storedString = localStorage.getItem('faves');
+        
+        if (storedString == null || storedString == 'null' || storedString == '') {
+            
+            storedString = '';
+        
+        } else {storedString += '%'}
+            
+        var stringToSave = storedString + urlToAdd;
+        
+        //add url onto end of stored string
+        localStorage.setItem('faves', stringToSave);
+        
+        console.log('item added. storage is now ' + localStorage.getItem('faves'));
+    }
+    
+    
+    function removeFromLocalStorage(urlToRemove) {
+        
+        //pull apart stored string
+        var pullApart = localStorage.getItem('faves').split('%');
+        
+        console.log('trying to remove ' + urlToRemove);
+        
+        //remove matching url string        
+        var pulledAgain  = pullApart.filter(function(elem){
+            return elem != urlToRemove; 
+        });
+        
+        //put it back together
+        var stringToSave = pulledAgain.join('%');
+        
+        //set new string
+        localStorage.setItem('faves', stringToSave);
+        
+        console.log('item should have been removed. storage is now ' + localStorage.getItem('faves'));
+    }
+    
+    
+    function unlockAllDigits() {
+        
+        for (var f = 0; f < digitArray.length; f++ ) {
+            
+            digitArray[f].locked = false;
+            digitArray[f].displaySlot.removeClass('digit-locked');
+        }
+    }
+    
+    /*
+    function rewriteLocalStorage() {
+        
+        //iterate through bookmark list
+        //create array of url strings
+        var newStoreArray = new Array();
+        
+        $('.bookmark-list').children().each(function(){
+            
+            console.log('bookmark item iteration');
+            
+            var anotherString = $(this).attr('date-fave-url');
+            newStoreArray.push(anotherString);
+        });
+        
+        localStorage.setItem('faves', newStoreArray.join('%'));
+        
+        console.log('storage is now ' + localStorage.getItem('faves'));
+    }
+    */
     
     //***************************************
     //*************  EVENTS
@@ -220,7 +354,58 @@ $(function(){
     });
     
     
+    addButton.click(function(){
+        
+        addNewBookmark(currentVideoNumber);
+        //rewriteLocalStorage();
+        addToLocalStorage(currentVideoNumber);
+    });
     
+    
+    bookmarkList.on('click','.bookmark-item .delete-button', function(){
+        
+        removeBookmark($(this));
+        //rewriteLocalStorage();
+        
+        console.log('delete icon is returning ' + $(this).attr('data-fave-url'));
+        
+        removeFromLocalStorage($(this).attr('data-fave-url'));
+        
+    });
+    
+    
+    bookmarkList.on('click', '.bookmark-item .fave-plunger', function(){
+       
+        event.preventDefault();
+        
+        var numFromFave = $(this).attr('href');
+            
+        previousVideoNumber = currentVideoNumber;
+        currentVideoNumber = numFromFave;
+        
+        //unlock all digits
+        unlockAllDigits();
+        
+        //update the actual text display with function 
+        updateTextDisplay(currentVideoNumber);
+        
+        //update vimeo frame with new digits in url
+        updateVimeoFrame(currentVideoNumber);
+    });
+    
+    
+    $('#clear-storage-button').click(function(){
+        
+        localStorage.clear();
+        console.log('storage is now ' + localStorage.getItem('faves'));
+    });
+    
+    /*
+    $(document).on('load', '.bookmark-item', function(){
+        
+        
+    });
+    */
     
     //****** end of document ready block
     //**** GET OUTTA HERE
